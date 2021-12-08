38ccac85d8297596c4ccdf49ba244c50903d4839
Sh4lok <95446489+Sh4lok@users.noreply.github.com>
2021-12-08 10:11:46 +0500

    new site

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="style.css">
+    
+    <title>Трансформер. Не плати за лишнее</title>
+</head>
+<body>
+
+
+
+
+
+
+</body>
+</html>
